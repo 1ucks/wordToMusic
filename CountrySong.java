aacf7fe07c352b0f8f0c192dce6b0d90413d8501
Kieran <112533380+GiveMeMoreSpleens@users.noreply.github.com>
2023-05-09 09:23:53 -0700

    Added the Song Types

---
+++ CountrySong.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import org.jfugue.player.Player;
+import org.jfugue.rhythm.Rhythm;
+import java.util.*;
+import org.jfugue.pattern.Pattern;
+
+public class CountrySong extends Song{
+	private ArrayList<String> Drumline = new ArrayList<String>(Arrays.asList("A5", "", "D5", "G5"));
+	public CountrySong(int howLong, int mostCommonColour, int averageColour, ArrayList<String> theChords) {
+		super(howLong,  mostCommonColour,  averageColour, theChords);
+	}
+	//the drumlines
+	public  Pattern generateDrumline() {
+		
+	}
+	public  Pattern generateDrumlineSwitchup() {
+		
+	}
+	
+	//the melodies
+	public String generateIntroMelody(ArrayList<String> keys) {
+		
+	}
+	public String generateChorusMelody(ArrayList<String> keys) {
+		
+	}
+	public String generateVerse1Melody(ArrayList<String> keys) {
+		
+	}
+	public String generateBridgeMelody(ArrayList<String> keys) {
+		
+	}
+	public String generateOutroMelody(ArrayList<String> keys) {
+		
+	}
+	
+	//the basslines
+	public String generateIntroBassLine(ArrayList<String> keys) {
+		
+	}
+	public String generateChorusBassLine(ArrayList<String> keys) {
+		
+	}
+	public String generateVerse1BassLine(ArrayList<String> keys) {
+		
+	}
+	public String generateBridgeBassLine(ArrayList<String> keys) {
+		
+	}
+	public String generateOutroBassLine(ArrayList<String> keys) {
+		
+	}
+	
+	//the background melodies
+	public String generateIntroBackground(ArrayList<String> keys) {
+		
+	}
+	public String generateChorusBackground(ArrayList<String> keys) {
+		
+	}
+	public String generateVerse1Background(ArrayList<String> keys) {
+		
+	}
+	public String generateBridgeBackground(ArrayList<String> keys) {
+		
+	}
+	public String generateOutroBackground(ArrayList<String> keys) {
+		
+	}
+}
