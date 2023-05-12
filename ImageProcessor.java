97a0b8d4b24168cea9e6b20b15ebeeb73f79518f
1ucks <112440142+1ucks@users.noreply.github.com>
2023-05-12 13:28:29 -0700

    added average and least common color methods

--- ImageProcessor.java
+++ ImageProcessor.java
@@ -162,9 +162,188 @@ public class ImageProcessor {
     	return "fail";
     	
     }
+    public String getLeastColor() throws IOException {
+    	//returns a string of the most common color in the image in all lower case
+    	
+    	//getting the image from the link
+    	BufferedImage im = ImageIO.read(this.imageUrl);
+    	
+    	
+    	//cycling through every pixel, skipping every other if its large
+    	int inc = 1;
+		int imArea = im.getHeight() * im.getWidth();
+    	if(im.getWidth() > 1000 || im.getHeight() > 1000) {
+    		inc = 2;
+    	}
+    	
+    	int numO = 0;
+    	int numY = 0;
+    	int numB = 0;
+    	int numG = 0;
+    	int numP = 0;
+    	int numR= 0;
+
+    	for(int i = 0; i < im.getHeight(); i+=inc) {
+    		
+    		for(int j = 0; j < im.getWidth(); j += inc) {
+    			//getting the rgb for the current pixel
+    			int pixelUno = im.getRGB(j, i);
+    			Color colorUno = new Color(pixelUno, true);
+    			
+    			int r = colorUno.getRed();
+    			int b = colorUno.getBlue();
+    			int g = colorUno.getGreen();
+    			
+    			//converting the rgb to hsb which lets us determine which color we are working with
+    			float[] HSB = new float[3];
+    			Color.RGBtoHSB(r, g, b, HSB);
+    			
+    			float hue = HSB[0]*360;
+    			//setting the color w/ its name
+    			if(hue < 15 || hue >= 345) {
+    				numR++;
+    			}
+    			else if(hue >= 15 && hue < 45) {
+    				numO++;
+    			}
+    			else if(hue >= 45 && hue < 75) {
+    				numY++;
+    			}
+    			else if(hue >= 75 && hue < 145) {
+    				numG++;
+    			}
+    			else if(hue >= 146 && hue < 250) {
+    				numB++;
+    			}
+    			else if(hue>= 250 && hue < 345) {
+    				numP++;
+    			}
+    			
+    		}
+    	}
+    	//determining the least common color
+    	int[] colors = {numR, numO, numY, numG, numB, numP};
+    	int min = colors[0];
+    	int count=0;
+    	int ind = 0;
+    	
+    	for(int i : colors) {
+    		if(i < min && i!= 0) {
+    			min = i;
+    			ind = count;
+    		}
+    		count++;
+    	}
+    	
+    	//returning
+    	switch(ind) {
+	    	case 0:
+	    		return "red";
+	    	case 1:
+	    		return "orange";
+	    	case 2:
+	    		return "yellow";
+	    	case 3:
+	    		return "green";
+	    	case 4:
+	    		return "blue";
+	    	case 5:
+	    		return "purple";
+	    	}
+    	System.out.println("fail");
+    	return "fail";
+    
+    }
+    public String getAverageColor() throws IOException {
+    	//returns a string of the most common color in the image in all lower case
+    	
+    	//getting the image from the link
+    	BufferedImage im = ImageIO.read(this.imageUrl);
+    	
+    	
+    	//cycling through every pixel, skipping every other if its large
+    	int inc = 1;
+		int imArea = im.getHeight() * im.getWidth();
+    	if(im.getWidth() > 1000 || im.getHeight() > 1000) {
+    		inc = 2;
+    	}
+    	
+    	double total = 0.0;
+    	int numPixels =0;
+    	
+    	
+    	
+    	
+    	for(int i = 0; i < im.getHeight(); i+=inc) {
+    		
+    		for(int j = 0; j < im.getWidth(); j += inc) {
+    			//getting the rgb for the current pixel
+    			int pixelUno = im.getRGB(j, i);
+    			Color colorUno = new Color(pixelUno, true);
+    			
+    			int r = colorUno.getRed();
+    			int b = colorUno.getBlue();
+    			int g = colorUno.getGreen();
+    			
+    			//converting the rgb to hsb which lets us determine which color we are working with
+    			float[] HSB = new float[3];
+    			Color.RGBtoHSB(r, g, b, HSB);
+    			
+    			float hue = HSB[0]*360;
+
+    			total+= hue;
+    			numPixels++;
+    			
+    			
+    		}
+    	}
+    	//averaging
+    	total/=numPixels;
+    	
+    	
+    	if(total < 15 || total >= 345) {
+			return "red";
+		}
+		else if(total >= 15 && total < 45) {
+			return "orange";
+		}
+		else if(total >= 45 && total < 75) {
+			return "yellow";
+		}
+		else if(total >= 75 && total < 145) {
+			return "green";
+		}
+		else if(total >= 146 && total < 250) {
+			return "blue";
+		}
+		else if(total>= 250 && total < 345) {
+			return "purple";
+		}
+    	System.out.println("fail");
+    	return "fail";
+    
+    }
     
     public void printImage() throws IOException {
     	//method to print the image chosen in a new window
+    	
+    	//getting the image from the link
+		BufferedImage im = ImageIO.read(this.imageUrl);
+		
+		Image im2 = im.getScaledInstance(im.getWidth() / 2, im.getHeight() / 2, Image.SCALE_DEFAULT);
+		//opens the window with the image v
+		JFrame f = new JFrame("Image Display");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//adds image to f
+		f.getContentPane().add(new JLabel(new ImageIcon(im2)));
+		f.pack();
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+		
+		
+    }
+    public void printImage(String domC, String recC) throws IOException {
+    	//overloaded method to print the image chosen in a new window
     	
     	//getting the image from the link
 		BufferedImage im = ImageIO.read(this.imageUrl);
