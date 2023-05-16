919995ebd565b3d1757c6097fc76c2f9dec07d56
Kieran <112533380+GiveMeMoreSpleens@users.noreply.github.com>
2023-05-15 23:17:54 -0700

    made the generat intro melody class, it works sometimes, can troublshoot in hello world

--- RockSong.java
+++ RockSong.java
@@ -5,10 +5,16 @@ import org.jfugue.pattern.Pattern;
 
 public class RockSong extends Song {
 	// constructor
-	public RockSong(int howLong, int mostCommonColour, int averageColour, ArrayList<String> theChords) {
-		super(howLong, mostCommonColour, averageColour, theChords);
+	public RockSong( int mostCommonColour, int averageColour, ArrayList<String> theChords) {
+		super(mostCommonColour, averageColour, theChords);
 	}
 
+	// the array of melodic rock instruments
+	private static String[] melodicInstruments = new String[] { "OVERDRIVEN_GUITAR", "GUITAR_FRET_NOISE", "STEEL_STRING_GUITAR",
+			"DISTORTION_GUITAR", "HARMONICA", "ROCK_ORGAN", "TANGO_ACCORDIAN", "BARITONE_SAX", "SKAKUHACHI",
+			"BLOWN_BOTTLE", "DULCIMER", "GUNSHOT", "SHANAI" };
+	//list of all pitches possible in assending order
+	private static String[] pitches = new String[] {"Ab", "A", "Bb", "B", "C", "C#", "D", "Eb", "E", "F", "F#", "G"};
 	// the lengths of each note
 	private static double[] noteLengths = new double[] { 4, 3, 2, 1.5, 1, 0.75, 0.5, 0.375, 0.25, 0.125 };
 	// the name of each note at the index
@@ -175,9 +181,95 @@ public class RockSong extends Song {
 
 	// the melodies
 	public String generateIntroMelody(ArrayList<String> keys) {
-
+		//intro will be 4 measures aka 2 drum switchups for intrest with a key switch in measure 3
+		int key1;
+		int key2;
+		if(keys == null) {
+			key1 = (int)(Math.random()*11);
+			key2 = (int)(Math.random()*11);
+		}else {
+			if(keys.size() == 1) {
+				int index;
+				for(index =0; index<pitches.length; index +=1) {
+					if(pitches[index] == keys.get(0)) {
+						break;
+					}
+				}
+				key1 = index;
+				key2 = (int)(Math.random()*11); 
+			}else {
+				int index;
+				for(index =0; index<pitches.length; index +=1) {
+					if(pitches[index] == keys.get(0)) {
+						break;
+					}
+				}
+				key1 = index;
+				keys.add(pitches[key1]);
+				keys.remove(0);
+				for(index =0; index<pitches.length; index +=1) {
+					if(pitches[index] == keys.get(0)) {
+						break;
+					}
+				}
+				key2 = index;
+				keys.add(pitches[key2]);
+				keys.remove(0);
+			}
+		}
+		String theMelody = "";
+		String thePattern = "";
+		switch(getFeel()){
+		case(0):
+			//angry chords: idk probably like 1/2step up or down to create discordanent sounds
+				for(int measureCount = 1; measureCount<=4; measureCount +=1) {
+					if (measureCount ==3) {
+						key1 = key2;
+					}
+					double beatCount = 0; 
+					int octave = (int)(Math.random()*4 + 5); 										
+					while(beatCount<4) { 															
+						// picks a random beat that fits in the measure
+						int theRandom = (int) (Math.random() * noteLengths.length);
+						// if it does not fit it adds it skips and tries again
+						if (beatCount + noteLengths[theRandom] > 4) {
+							continue;
+						}
+						thePattern += pitches[key1] + "" + octave + noteNames[theRandom];
+						int nextNote = ((int)(Math.random()*7))*2 -7;
+						if(key1 + nextNote >= pitches.length) {
+							octave +=1;
+							key1 = key1 + nextNote - pitches.length;
+						}
+						//increment beatCount
+						beatCount += noteLengths[theRandom];
+					}
+					thePattern += " ";
+				}
+			for(int count = 0; count < 4; count +=1) {
+				theMelody += "V" + count + " I[" + melodicInstruments[(int)(Math.random()*13)] + "] " +thePattern + "\n"; 
+			}
+			break;
+		case(1):
+			
+			break;
+		case(2):
+			
+			break;
+		case(3):
+			
+			break;
+		case(4):
+			
+			break;
+		case(5):
+			
+			break;
+		default:
+			theMelody = "";
+		}
+		return theMelody;
 	}
-
 	public String generateChorusMelody(ArrayList<String> keys) {
 
 	}
